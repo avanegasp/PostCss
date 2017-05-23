5153c8051865aaebf45eb414d2fb8284580cd433
Angie Vanegas <arco527@gmail.com>
2017-05-23 18:36:59 -0500

    Add pluggin css wring

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var cssnext = require('postcss-cssnext');
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var rucksack = require('gulp-rucksack')
+var cssnext = require('postcss-cssnext')
 // var autoprefixer = require('autoprefixer');
-var cssnested = require('postcss-nested');
-var mixins = require('postcss-mixins');
-var lost = require('lost');
-var atImport = require('postcss-import');
-var browserSync = require('browser-sync').create();
+var cssnested = require('postcss-nested')
+var mixins = require('postcss-mixins')
+var lost = require('lost')
+var atImport = require('postcss-import')
+var csswring = require('csswring')
+var browserSync = require('browser-sync').create()
+
 
 
 // Servidor de desarrollo
@@ -26,10 +29,12 @@ gulp.task('css', function(){
   var processors =[
     atImport(),
     mixins(),
-    lost(),
-    // autoprefixer({ browsers: ['>5%', 'ie 8']}),
     cssnested, // primero que cssnext
-    cssnext({browsers: ['>5%', 'ie 8']}) // ya contiene autoprefixer por eso o borraré
+    // autoprefixer({ browsers: ['>5%', 'ie 8']}),
+    lost(),
+    rucksack(),
+    cssnext({browsers: ['>5%', 'ie 8']}), // ya contiene autoprefixer por eso o borraré
+    csswring()
   ]
 
   return gulp.src('./src/invie.css')
